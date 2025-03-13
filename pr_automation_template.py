10f8b20351dd1cba582222ce2fceeac6807efb47
Filipe Schenkel <sfschenkel@gmail.com>
2025-03-13 14:46:43 +0100

    Create pr_automation_template.py

---
+++ pr_automation_template.py
@@ -0,0 +1,9 @@
+class PRAutomationTemplate():
+    def pr_open(self, github_env: dict):
+        pass
+
+    def pr_updated(self, github_env: dict):
+        pass
+
+    def pr_closed(self, github_env: dict):
+        pass

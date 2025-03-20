6eef2a31cd7672a33d2b30d6feeb4bef47c7e74c
Filipe Schenkel <sfschenkel@gmail.com>
2025-03-20 23:32:42 +0100

    listing pys

--- automation_entry_point.py
+++ automation_entry_point.py
@@ -4,7 +4,7 @@ import inspect
 import importlib
 
 sys.path.insert(0, "automation")
-automation_module = importlib.import_module("automation")
+#automation_module = importlib.import_module("automation")
 #framework_module = importlib.import_module("framework.pr_automation_template")
 
 #from framework_module import PRAutomationTemplate
@@ -13,7 +13,9 @@ from framework.pr_automation_template import PRAutomationTemplate
 if __name__ == "__main__":
     print(f"current dir is: {os.getcwd()}")
 
-    #subcls = [cls for _, cls in inspect.getmembers(automation_module, inspect.isclass) if issubclass(cls, framework_module.PRAutomationTemplate)]
-    subcls = [cls for _, cls in inspect.getmembers(automation_module, inspect.isclass) if issubclass(cls, PRAutomationTemplate)]
+    for module in glob.iglob("*.py"):
+        print(f"Found module: {module}")
 
-    print(f"subclasses are: {str(subcls)}")
+    #subcls = [cls for _, cls in inspect.getmembers(automation_module, inspect.isclass) if issubclass(cls, framework_module.PRAutomationTemplate)]
+    #subcls = [cls for _, cls in inspect.getmembers(automation_module, inspect.isclass) if issubclass(cls, PRAutomationTemplate)]
+    #print(f"subclasses are: {str(subcls)}")

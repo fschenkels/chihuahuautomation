274513b9c7cfa3ad1028c773c29f4d0f4500045b
Filipe Schenkel <sfschenkel@gmail.com>
2025-03-21 00:00:50 +0100

    list prsubclasses

--- automation_entry_point.py
+++ automation_entry_point.py
@@ -10,17 +10,21 @@ from framework.pr_automation_template import PRAutomationTemplate
 if __name__ == "__main__":
     print(f"current dir is: {os.getcwd()}")
 
+    pr_subclasses = list()
     imported_modules = list()
     for module in glob.iglob("*.py", root_dir="automation"):
         print(f"Found module: {module}")
+        imported_module = importlib.import_module(
+            module.replace(".py", "")
+        )
         imported_modules.append(
-            importlib.import_module(module.replace(".py", ""))
+            imported_module
         )
 
-    pr_subclasses = [
-        cls for _, cls in inspect.getmembers(
-            automation_module, inspect.isclass
-        ) if issubclass(cls, PRAutomationTemplate)
-    ]
+        pr_subclasses.extend([
+            cls for _, cls in inspect.getmembers(
+                imported_module, inspect.isclass
+            ) if issubclass(cls, PRAutomationTemplate)
+        ])
 
     print(f"subclasses are: {str(pr_subclasses)}")

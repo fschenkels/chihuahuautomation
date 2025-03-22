d1db83ae735fba43c73164d076205810b7adc791
Filipe Schenkel <sfschenkel@gmail.com>
2025-03-22 12:37:42 +0100

    static class

--- automation_entry_point.py
+++ automation_entry_point.py
@@ -18,36 +18,38 @@ sys.path.insert(0, USER_FOLDER)
 # imports the ABC meant as interface with the user's code
 from framework.pr_automation_template import PRAutomationTemplate
 
-def import_users_concrete_implementations(abcs: list):
-    """Returns a list of touples containing the user's concrete classes and
-    their respective modules"""
-    classes_and_modules = list()
+class UsersCodeParser:
+    @staticmethod
+    def parse(abcs: list):
+        """Returns a list of touples containing the user's concrete classes and
+        their respective modules"""
+        classes_and_modules = list()
 
-    logging.info(f"Importing concrete implementations from '{USER_FOLDER}'...")
-    for module in glob.iglob("*.py", root_dir=USER_FOLDER):
-        logging.debug(f"Found module: {module}")
-        imported_module = importlib.import_module(
-            module.replace(".py", "")
-        )
+        logging.info(f"Importing concrete implementations from '{USER_FOLDER}'...")
+        for module in glob.iglob("*.py", root_dir=USER_FOLDER):
+            logging.debug(f"Found module: {module}")
+            imported_module = importlib.import_module(
+                module.replace(".py", "")
+            )
 
-        for abc in abcs:
-            logging.debug(f"reading implementations of {abc}...")
-            implementations_found = [
-                (cls, imported_module) for _, cls in inspect.getmembers(
-                    imported_module, inspect.isclass
-                ) if issubclass(cls, abc) and not cls is abc
-            ]
+            for abc in abcs:
+                logging.debug(f"reading implementations of {abc}...")
+                implementations_found = [
+                    (cls, imported_module) for _, cls in inspect.getmembers(
+                        imported_module, inspect.isclass
+                    ) if issubclass(cls, abc) and not cls is abc
+                ]
 
-            logging.debug(f"implementations found: {str(implementations_found)}")
-            classes_and_modules.extend(implementations_found)
+                logging.debug(f"implementations found: {str(implementations_found)}")
+                classes_and_modules.extend(implementations_found)
 
-    return classes_and_modules
+        return classes_and_modules
 
 
 if __name__ == "__main__":
     logging.debug(f"current dir is: {os.getcwd()}")
 
-    for concrete_class, _ in import_users_concrete_implementations(
+    for concrete_class, _ in UsersCodeParser.parse(
         [PRAutomationTemplate]
     ):
         logging.info(

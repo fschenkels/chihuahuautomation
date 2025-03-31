d4bf46ad298a8687ec4096098a5622b2fb7efb53
Filipe Schenkel <sfschenkel@gmail.com>
2025-03-31 12:45:36 +0200

    Create better-classes.py
    some nice ideas

---
+++ better-classes.py
@@ -0,0 +1,65 @@
+@dataclass
+class Event:
+    context: dict
+
+    def define_event_type(self):
+        pass
+
+    @property
+    def is_push(self):
+        self.define_event_type()
+        return self.type == "push"
+
+    @property
+    def is_pr(self):
+        self.define_event_type()
+        return self.type == "pr"
+
+    @property
+    def is_scheduled(self):
+        self.define_event_type()
+        return self.type == "scheduled"
+
+class GithubEvent(Event)
+    pass
+
+class GitlabEvent(Event)
+    pass
+
+class AzureEvent(Event)
+    pass
+
+
+class EventsFactory:
+    def __init__(self):
+        self.vendor = self.__verify_vendors_environment()
+        self.available_events = {
+            "github": GithubEvent,
+            "gitlab": GitlabEvent,
+            "azure": AzureEvent
+        }
+
+    def __verify_vendors_environment(self):
+        # maybe a routine for figuring out the vendor from the 
+        # execution environment at the startup
+        return False
+
+    def __extract_vendor_from_context(self, context)
+        pass
+
+    def generate_event(self, context):
+        if not self.vendor:
+            self.vendor = self.__extract_vendor_from_context(context)
+        return self.available_events[self.vendor]()
+
+class EventsProcessor:
+    def __init__(self):
+        self.userscode = UsersCodeParser()
+
+    def process(self, event):
+        if event.is_pr:
+            classes_to_run = self.userscode.get_implementations_of(
+                [PRAutomationTemplate]
+            )
+        for cls in classes_to_run:
+            cls.run()
